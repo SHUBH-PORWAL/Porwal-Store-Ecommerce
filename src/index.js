644432fb9e9c619d3a25ff5863611728896f7368
SHUBH PORWAL <114678897+SHUBH-PORWAL@users.noreply.github.com>
2023-09-02 21:33:36 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { AppProvider } from "./context/productcontext";
+import {FilterContextProvider} from "./context/filter_context"
+import { CartProvider } from "./context/cart_context";
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <Auth0Provider
+  domain="dev-7nja8thxekfqysxm.us.auth0.com"
+  clientId="OXFyJeTFDqjF5vKb1p4U7G0wbTQaCf0V"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+    <AppProvider>
+      <FilterContextProvider>
+        <CartProvider>
+      <App />
+        </CartProvider>
+      </FilterContextProvider>
+    </AppProvider>
+    </Auth0Provider>
+    );
+ reportWebVitals(); 
